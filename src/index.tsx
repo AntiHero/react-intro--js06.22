7f8e49db192baaab7ea4b23e24a8daaf96f492fd
Aleksandr Schemelev <prophreaker@gmail.com>
2022-11-30 13:59:30 +0300

    initial

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLDivElement
+);
+root.render(<h1>Hello, world!</h1>);
